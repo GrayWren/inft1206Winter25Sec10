8e7b6c20128087626506950482c7478937512a0d
GrayWren <rileykelm2002@gmail.com>
2025-03-10 17:37:14 -0400

    g

--- js/w9ex.js
+++ js/w9ex.js
@@ -4,4 +4,5 @@ function showGreetingMessage(){
     let name =window.prompt("What is your name?");
     window.alert("Hello "+name)
 }
-showGreetingMessage();
+//showGreetingMessage();
+document.querySelector("#btn").addEventListener("click",showGreetingMessage);
