2403342841c00bf65d8cc41232722b37c26ef1db
GrayWren <rileykelm2002@gmail.com>
2025-03-10 18:57:59 -0400

    part 1 complete

--- js/Assingment4-js/part1.js
+++ js/Assingment4-js/part1.js
@@ -2,13 +2,14 @@ console.log("Hello")
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
+console.log(story);
 
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
 
-let storyText="It was 94 fahrenheit outside, so "+xItem+" went for a walk. When they got to "+yItem+" , they stared in horror for a few moments, then "+zItem+". Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+let storyText="It was 94 fahrenheit outside, so xItem went for a walk. When they got to yItem, they stared in horror for a few moments, then zItem. Bob saw the whole thing, but was not surprised xItem weighs 300 pounds, and it was a hot day."
 const insertx=["Willy the Goblin" ," Big Daddy" ," Father Christmas" ]
 const inserty=["the soup kitchen", "Disneyland","the White House"]
 const insertz=["spontaneously combusted","melted into a puddle on the sidewalk","turned into a slug and crawled away"]
@@ -20,15 +21,23 @@ let newStory=storyText
 let xItem=randomValueFromArray(insertx)
 let yItem=randomValueFromArray(inserty)
 let zItem=randomValueFromArray(insertz)
+newStory=newStory.replaceAll("xItem",xItem)
+
+newStory=newStory.replace("yItem",yItem)
+
+newStory=newStory.replace("zItem",zItem)
+
     if(customName.value !== '') {
       const name = customName.value;
-      storyText.replace('bob',name)
+      newStory=newStory.replace('Bob',name)
   
     }
   
     if(document.getElementById("uk").checked) {
       const weight = Math.round(300/14)+" stones";
+      newStory=newStory.replace("94 fahrenheit",weight)
       const temperature =  Math.round((94 -32) * 5/9 )+" centigrade";
+      newStory=newStory.replace("300 pounds",temperature)
   
     }
   
