529e7fbb485b5b19462345de778fd95f43f8c99f
GrayWren <rileykelm2002@gmail.com>
2025-03-10 18:36:11 -0400

    part 1 update

--- js/Assingment4-js/part1.js
+++ js/Assingment4-js/part1.js
@@ -8,26 +8,30 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyname="It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
-const insertx=["Willythe Goblin" ," Big Daddy" ," Father Christmas" ]
+let storyText="It was 94 fahrenheit outside, so "+xItem+" went for a walk. When they got to "+yItem+" , they stared in horror for a few moments, then "+zItem+". Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+const insertx=["Willy the Goblin" ," Big Daddy" ," Father Christmas" ]
 const inserty=["the soup kitchen", "Disneyland","the White House"]
 const insertz=["spontaneously combusted","melted into a puddle on the sidewalk","turned into a slug and crawled away"]
 
 randomize.addEventListener('click', result);
 
 function result() {
-
+let newStory=storyText
+let xItem=randomValueFromArray(insertx)
+let yItem=randomValueFromArray(inserty)
+let zItem=randomValueFromArray(insertz)
     if(customName.value !== '') {
       const name = customName.value;
+      storyText.replace('bob',name)
   
     }
   
     if(document.getElementById("uk").checked) {
-      const weight = Math.round(300);
-      const temperature =  Math.round(94);
+      const weight = Math.round(300/14)+" stones";
+      const temperature =  Math.round((94 -32) * 5/9 )+" centigrade";
   
     }
   
-    story.textContent = ;
+    story.textContent = newStory;
     story.style.visibility = 'visible';
   }
